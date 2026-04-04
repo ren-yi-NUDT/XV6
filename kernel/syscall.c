3012289cfb2defd667cde79ee83cd874f1f84945
Ren Yi <renyi_24@nudt.edu.cn>
2026-04-04 20:42:34 +0800

    Finish Using GDB

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -133,7 +133,6 @@ syscall(void)
 {
   int num;
   struct proc *p = myproc();
-
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     // Use num to lookup the system call function for num, call it,
