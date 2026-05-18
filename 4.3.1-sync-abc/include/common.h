e0dd75d1de6657295c48219b625a460f698b6882
Reny <renyi_24@nudt.edu.cn>
2026-05-18 10:28:48 +0800

    原理实验4.3.1题目上传

---
+++ 4.3.1-sync-abc/include/common.h
@@ -0,0 +1,22 @@
+#ifndef __common_h__
+#define __common_h__
+
+#include <assert.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+
+static double GetTime() {
+  struct timeval t;
+  int rc = gettimeofday(&t, NULL);
+  assert(rc == 0);
+  return (double)t.tv_sec + (double)t.tv_usec / 1e6;
+}
+
+static inline void Spin(double howlong) {
+  double t = GetTime();
+  while ((GetTime() - t) < howlong)
+    ; // do nothing in loop
+}
+
+#endif // __common_h__
